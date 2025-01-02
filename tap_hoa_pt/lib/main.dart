0e0b9353fa4cd546ed6f754b266a5593f8950639
Cab6701 <trinhgiachang@gmail.com>
2025-01-03 00:27:05 +0700

    add fonts

--- tap_hoa_pt/lib/main.dart
+++ tap_hoa_pt/lib/main.dart
@@ -9,12 +9,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Tap Hoa Phuong Thuy"),
         ),
+        body: Body(),
       ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      child: TextDemo(),
+    ));
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  const TextDemo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      overflow: TextOverflow.ellipsis,
+      maxLines: 10,
+      textScaler: TextScaler.linear(1.0),
+      style: TextStyle(
+          color: Colors.pinkAccent,
+          fontFamily: "Merriweather",
+          fontSize: 18,
+          fontStyle: FontStyle.italic),
     );
   }
 }
